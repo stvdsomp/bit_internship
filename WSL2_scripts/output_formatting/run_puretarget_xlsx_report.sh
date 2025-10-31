ca0e7d4288e803ac7c2a58cbaeecd7e91b3e8ffc
stvdsomp <53609680+stvdsomp@users.noreply.github.com>
2025-10-31 16:21:07 +0100

    improved run_puretarget_xlsx_report.sh

--- WSL2_scripts/output_formatting/run_puretarget_xlsx_report.sh
+++ WSL2_scripts/output_formatting/run_puretarget_xlsx_report.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-script_dir = "/home/svdsompe/BIT11/sync/WSL2_scripts/output_formatting"
-output_dir = "/home/svdsompe/BIT11/local_data/r84299_20250623_141348"
-samplesheet = "$OUTPUT_DIR/samplesheet.csv"
+# Define variables
+my_env=bit11
+script_dir="/home/svdsompe/BIT11/sync/WSL2_scripts/output_formatting"
+output_dir="/home/svdsompe/BIT11/local_data/r84299_20250623_141348"
+samplesheet="$output_dir/samplesheet.csv"
 
-conda activate bit11
-python3 script_dir/generate_puretarget_xlsx_report.py $output_dir $samplesheet
+# Run the script inside the conda environment
+conda run -n "$my_env" python3 "$script_dir/generate_puretarget_xlsx_report.py" "$output_dir" "$samplesheet"
