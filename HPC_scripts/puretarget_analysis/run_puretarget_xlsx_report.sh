8f65a78119822a48cb592b93c65a6189569bc1cb
vsc42287 vscuser <vsc42287@gligar09.gastly.os>
2025-10-31 16:09:33 +0100

    clean up HPC directory

--- HPC_scripts/puretarget_analysis/run_puretarget_xlsx_report.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load required modules
-module load SciPy-bundle/2025.07-gfbf-2025b 
-module load openpyxl/3.1.5-GCCcore-13.3.0
-
-# Run your Python script
-python generate_puretarget_xlsx_report.py
