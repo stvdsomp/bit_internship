25b289dbcfed06324abdf1cd1ab325533ff415f4
vsc42287 vscuser <vsc42287@gligar09.gastly.os>
2025-11-13 16:08:25 +0100

    update run_nf_pacvar.sh

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#module swap cluster/donphan
+#sinfo
+#qsub -I -l nodes=1:ppn=8,mem=24gb,walltime=03:00:00
+
 nextflow run stvdsomp/pacvar \
    -r dev_personal \
    -latest \
@@ -12,6 +16,5 @@ nextflow run stvdsomp/pacvar \
    --skip_demultiplexing true \
    --intervals /kyukon/data/gent/vo/000/gvo00082/research/ICT/VAL/Revio_PureTarget/bin/roi/PureTarget_repeat_expansion_panel_1.0.repeat_definition.GRCh38.bed \
    --workflow repeat \
-   --repeat_id C9ORF72
-  #--repeat_id "AR,ATN1,ATXN1,ATXN10,ATXN2,ATXN3,ATXN7,ATXN8,C9ORF72,CACNA1A,CNBP,DMPK,FMR1,FXN,HTT,PABPN1,PPP2R2B,RFC1,TBP,TCF4"
+  --repeat_id C9ORF72
   #--cleanup False 
