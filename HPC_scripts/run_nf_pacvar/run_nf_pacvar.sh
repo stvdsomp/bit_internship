78c467eaa6f4c22b152cf42dcac9a1f30a1f2444
vsc42287 vscuser <vsc42287@node4010.donphan.os>
2025-11-14 16:32:20 +0100

    add .gitignore file

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
@@ -9,12 +9,11 @@ nextflow run stvdsomp/pacvar \
    -latest \
    -profile vsc_ugent \
    -process.executor local \
-   --input samplesheet.csv \
-   --outdir ~/BIT11/local_output/pacvar_personal_FXN \
+   --input samplesheet_single.csv \
+   --outdir ~/BIT11/local_output/pacvar_personal_C9ORF72 \
    --fasta /kyukon/data/gent/vo/000/gvo00082/research/ICT/VAL/Revio_PureTarget/hg38-noalt.fa \
    --fasta_fai /kyukon/data/gent/vo/000/gvo00082/research/ICT/VAL/Revio_PureTarget/hg38-noalt.fa.fai \
    --skip_demultiplexing true \
    --intervals /kyukon/data/gent/vo/000/gvo00082/research/ICT/VAL/Revio_PureTarget/bin/roi/PureTarget_repeat_expansion_panel_1.0.repeat_definition.GRCh38.bed \
    --workflow repeat \
-  --repeat_id FXN
-  #--cleanup False 
+   --repeat_id C9ORF72
