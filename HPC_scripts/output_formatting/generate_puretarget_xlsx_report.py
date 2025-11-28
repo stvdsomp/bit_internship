2e443a154ea413044cbe3ee6ed7710431450376d
vsc42287 vscuser <vsc42287@gligar09.gastly.os>
2025-11-28 10:47:06 +0100

    allow generation of multiple reports per sample

--- HPC_scripts/output_formatting/generate_puretarget_xlsx_report.py
+++ HPC_scripts/output_formatting/generate_puretarget_xlsx_report.py
@@ -51,188 +51,185 @@ def color_motifs_in_sequence(seq, motifs, header):
             i += 1
     return colored_seq
 
-def write_report(samples,output_dir):  
+def write_report(sample_name, repeat_of_interest, output_dir):  
 
     os.makedirs(os.path.join(output_dir, "output_report"), exist_ok=True)
 
-    for sample in samples:
-        sample_name = sample['sample']
-        repeat_of_interest = sample['roi']
-        filepath = os.path.join(output_dir, "trgt")
-        vcf_file = os.path.join(filepath, f"{sample_name}.vcf.gz")
-        if repeat_of_interest:
-            output_excel = os.path.join(output_dir, "output_report", f"PureTarget_{sample_name}_{repeat_of_interest}.xlsx")
-        else:
-            output_excel = os.path.join(output_dir, "output_report", f"PureTarget_{sample_name}_all_repeats.xlsx")
+    filepath = os.path.join(output_dir, "trgt")
+    vcf_file = os.path.join(filepath, f"{sample_name}.vcf.gz")
+    if repeat_of_interest:
+        output_excel = os.path.join(output_dir, "output_report", f"PureTarget_{sample_name}_{repeat_of_interest}.xlsx")
+    else:
+        output_excel = os.path.join(output_dir, "output_report", f"PureTarget_{sample_name}_all_repeats.xlsx")
 
-        # Extract TRGT version from VCF
-        with gzip.open(vcf_file, 'rt') as f:
-            for line in f:
-                if "##trgtVersion=" in line:
-                    trgt_version = re.search(r'trgtVersion=(\S+)', line).group(1)
-                    trgt_version = trgt_version.split('-')[0]
-                    break  # stop loop once found
+    # Extract TRGT version from VCF
+    with gzip.open(vcf_file, 'rt') as f:
+        for line in f:
+            if "##trgtVersion=" in line:
+                trgt_version = re.search(r'trgtVersion=(\S+)', line).group(1)
+                trgt_version = trgt_version.split('-')[0]
+                break  # stop loop once found
 
-        # Parse VCF
-        vcf_data = []
-        with gzip.open(vcf_file, 'rt') as f:
-            for line in f:
-                if line.startswith("#"):
-                    continue
-                parts = line.strip().split('\t')
-                chrom, pos, _, ref, alt, _, _, info, fmt, smpl = parts
-                info_dict = dict([x.split('=') for x in info.split(';')])
-                fmt_keys = fmt.split(':')
-                smpl_values = smpl.split(':')
-                fmt_dict = dict(zip(fmt_keys, smpl_values))
-                vcf_data.append({
-                    "CHROM": chrom,
-                    "POS": pos,
-                    "REF": ref,
-                    "ALT": alt,
-                    "TRID": info_dict.get("TRID"),
-                    "MOTIFS": info_dict.get("MOTIFS"),
-                    "GT": fmt_dict.get("GT"),
-                    "AL": fmt_dict.get("AL"),
-                    "ALLR": fmt_dict.get("ALLR"),
-                    "SD": fmt_dict.get("SD"),
-                    "MC": fmt_dict.get("MC"),
-                    "MS": fmt_dict.get("MS"),
-                    "AP": fmt_dict.get("AP"),
-                    "AM": fmt_dict.get("AM")
-        })
-        vcf_df = pd.DataFrame(vcf_data)
+    # Parse VCF
+    vcf_data = []
+    with gzip.open(vcf_file, 'rt') as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            parts = line.strip().split('\t')
+            chrom, pos, _, ref, alt, _, _, info, fmt, smpl = parts
+            info_dict = dict([x.split('=') for x in info.split(';')])
+            fmt_keys = fmt.split(':')
+            smpl_values = smpl.split(':')
+            fmt_dict = dict(zip(fmt_keys, smpl_values))
+            vcf_data.append({
+                "CHROM": chrom,
+                "POS": pos,
+                "REF": ref,
+                "ALT": alt,
+                "TRID": info_dict.get("TRID"),
+                "MOTIFS": info_dict.get("MOTIFS"),
+                "GT": fmt_dict.get("GT"),
+                "AL": fmt_dict.get("AL"),
+                "ALLR": fmt_dict.get("ALLR"),
+                "SD": fmt_dict.get("SD"),
+                "MC": fmt_dict.get("MC"),
+                "MS": fmt_dict.get("MS"),
+                "AP": fmt_dict.get("AP"),
+                "AM": fmt_dict.get("AM")
+    })
+    vcf_df = pd.DataFrame(vcf_data)
 
-        # Create Excel workbook
-        wb = Workbook()
-        ws = wb.active
-        ws.title = "report"
+    # Create Excel workbook
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "report"
 
-        # Write header
-        ws.append(["PureTarget analysis report"])
-        ws.append([f"{sample_name}"])
-        ws.append([f"TRGT v{trgt_version}"])
+    # Write header
+    ws.append(["PureTarget analysis report"])
+    ws.append([f"{sample_name}"])
+    ws.append([f"TRGT v{trgt_version}"])
+    ws.append([])
+    cell = ws.cell(row=1, column=1)
+    cell.font = Font(name='Calibri', size=18, bold=True, color='FFE31B92')
+    bold_font = Font(name='Calibri', bold=True)
+    grey_fill = PatternFill(start_color='FFD3D3D3', end_color='FFD3D3D3', fill_type='solid')
+    for row in ws['A2:A3']:
+        for cell in row:
+            cell.font = bold_font
+
+    # Filter for specific repeat (if needed)
+    if repeat_of_interest:
+        vcf_df = vcf_df[vcf_df['TRID'] == repeat_of_interest]
+        if vcf_df.empty:
+            raise ValueError(
+                f"ERROR: repeat_id '{repeat_of_interest}' was not found in the analyzed panel.\n"
+                f"(Available TRIDs: {', '.join(sorted(vcf_df['TRID'].unique()))})"
+            )
+
+    # Write summary for repeat(s) of interest
+    for idx, row in vcf_df.iterrows():
+        ws.append(["Gene", row['TRID']])
+        ws.cell(row=ws.max_row, column=1).font = bold_font
+        ws.append(["Position", f"{row['CHROM']}:{row['POS']}"])
+        ws.cell(row=ws.max_row, column=1).font = bold_font
+
+        motifs_colored = color_motifs_in_sequence(seq=row['MOTIFS'], motifs=row['MOTIFS'].split(','), header=True)
+        ws.append(["Expected motifs", motifs_colored])
+        ws.cell(row=ws.max_row, column=1).font = bold_font
+
+        for row_idx in range(ws.max_row-2,ws.max_row+1):
+            for col_idx in range(1, 100):
+                ws.cell(row=row_idx, column=col_idx).fill = grey_fill
         ws.append([])
-        cell = ws.cell(row=1, column=1)
-        cell.font = Font(name='Calibri', size=18, bold=True, color='FFE31B92')
-        bold_font = Font(name='Calibri', bold=True)
-        grey_fill = PatternFill(start_color='FFD3D3D3', end_color='FFD3D3D3', fill_type='solid')
-        for row in ws['A2:A3']:
-            for cell in row:
-                cell.font = bold_font
+        ws.append(["Allele", "Length", "Length range", "Reads spanning allele", "Motif count", "Motif span", "Purity", "Mean methylation", "Sequence"])
+        for cell in ws[ws.max_row]:
+            cell.font = bold_font
 
-        # Filter for specific repeat (if needed)
-        if repeat_of_interest:
-            vcf_df = vcf_df[vcf_df['TRID'] == repeat_of_interest]
-            if vcf_df.empty:
-                raise ValueError(
-                    f"ERROR: repeat_id '{repeat_of_interest}' was not found in the analyzed panel.\n"
-                    f"(Available TRIDs: {', '.join(sorted(vcf_df['TRID'].unique()))})"
-                )
+        # Determine alleles based on GT
+        gt = row.get("GT", "")
+        ref = row["REF"]
+        alt_alleles = row["ALT"].split(",") if row["ALT"] else []
 
-        # Write summary for repeat(s) of interest
-        for idx, row in vcf_df.iterrows():
-            ws.append(["Gene", row['TRID']])
-            ws.cell(row=ws.max_row, column=1).font = bold_font
-            ws.append(["Position", f"{row['CHROM']}:{row['POS']}"])
-            ws.cell(row=ws.max_row, column=1).font = bold_font
+        if "/" in gt:
+            allele_indices = gt.split("/")
+        else:
+            # "0" or "1" for chrX
+            allele_indices = [gt]       
 
-            motifs_colored = color_motifs_in_sequence(seq=row['MOTIFS'], motifs=row['MOTIFS'].split(','), header=True)
-            ws.append(["Expected motifs", motifs_colored])
-            ws.cell(row=ws.max_row, column=1).font = bold_font
+        # Convert to integers (ignore '.')
+        allele_indices = [int(a) for a in allele_indices if a.isdigit()]   
 
-            for row_idx in range(ws.max_row-2,ws.max_row+1):
-                for col_idx in range(1, 100):
-                    ws.cell(row=row_idx, column=col_idx).fill = grey_fill
-            ws.append([])
-            ws.append(["Allele", "Length", "Length range", "Reads spanning allele", "Motif count", "Motif span", "Purity", "Mean methylation", "Sequence"])
-            for cell in ws[ws.max_row]:
-                cell.font = bold_font
+        # Helper to safely split list fields
+        def safe_split(value):
+            return value.split(",") if value else []
 
-            # Determine alleles based on GT
-            gt = row.get("GT", "")
-            ref = row["REF"]
-            alt_alleles = row["ALT"].split(",") if row["ALT"] else []
+        al_list  = safe_split(row.get("AL"))
+        allr_list = safe_split(row.get("ALLR"))
+        sd_list  = safe_split(row.get("SD"))
+        mc_list  = safe_split(row.get("MC"))
+        ms_list  = safe_split(row.get("MS"))
+        ap_list  = safe_split(row.get("AP"))
+        am_list  = safe_split(row.get("AM"))
 
-            if "/" in gt:
-                allele_indices = gt.split("/")
-            else:
-                # "0" or "1" for chrX
-                allele_indices = [gt]       
+        # Build a mapping
+            # 0:REF
+            # 1:ALT_1
+            # 2:ALT_2
+        allele_map = {0: ref}
+        for i, alt in enumerate(alt_alleles):
+            allele_map[i+1] = alt
 
-            # Convert to integers (ignore '.')
-            allele_indices = [int(a) for a in allele_indices if a.isdigit()]   
+        # Loop through alleles in GT and write one row per allele
+        for idx, allele_index in enumerate(allele_indices):
+            allele_seq = allele_map.get(allele_index, "")
+            allele_seq_colored = color_motifs_in_sequence(seq=allele_seq, motifs=row['MOTIFS'].split(','), header=False)
+            allele_label = f"Allele {idx+1}"            
+            row_cells = [
+                allele_label,
+                al_list[idx],
+                allr_list[idx],
+                sd_list[idx],
+                mc_list[idx],
+                ms_list[idx],
+                ap_list[idx],
+                am_list[idx],
+                allele_seq_colored
+            ]
+            ws.append(row_cells)
+        ws.append([])
+        ws.append([])
+        
+    # Final formatting of 'report' sheet
+    for col in ws.columns:
+        ws.column_dimensions[col[0].column_letter].width = 17
 
-            # Helper to safely split list fields
-            def safe_split(value):
-                return value.split(",") if value else []
+    # For each repeat, print images on separate sheet
+    for trid in sorted(vcf_df['TRID'].unique()):
+        # Excel sheet name max 31 chars
+        sheet_name = trid[:31]  
+        ws_trid = wb.create_sheet(title=sheet_name)
+        motif_png = os.path.join(filepath, f"{sample_name}_{trid}_motifs.png")
+        meth_png  = os.path.join(filepath, f"{sample_name}_{trid}_meth.png")
 
-            al_list  = safe_split(row.get("AL"))
-            allr_list = safe_split(row.get("ALLR"))
-            sd_list  = safe_split(row.get("SD"))
-            mc_list  = safe_split(row.get("MC"))
-            ms_list  = safe_split(row.get("MS"))
-            ap_list  = safe_split(row.get("AP"))
-            am_list  = safe_split(row.get("AM"))
+        # Define starting location
+        row_pos = 1
+        col_pos = 1
 
-            # Build a mapping
-                # 0:REF
-                # 1:ALT_1
-                # 2:ALT_2
-            allele_map = {0: ref}
-            for i, alt in enumerate(alt_alleles):
-                allele_map[i+1] = alt
+        for label, cell, png_path  in [("Motifs", "A3", motif_png), ("Methylation", "M3", meth_png)]:
+            if os.path.exists(png_path):
+                # Insert a label
+                ws_trid.cell(row=row_pos, column=col_pos, value=f"{label} {trid}")
+                ws_trid.cell(row=row_pos, column=col_pos).font = bold_font
+                col_pos += 12
 
-            # Loop through alleles in GT and write one row per allele
-            for idx, allele_index in enumerate(allele_indices):
-                allele_seq = allele_map.get(allele_index, "")
-                allele_seq_colored = color_motifs_in_sequence(seq=allele_seq, motifs=row['MOTIFS'].split(','), header=False)
-                allele_label = f"Allele {idx+1}"            
-                row_cells = [
-                    allele_label,
-                    al_list[idx],
-                    allr_list[idx],
-                    sd_list[idx],
-                    mc_list[idx],
-                    ms_list[idx],
-                    ap_list[idx],
-                    am_list[idx],
-                    allele_seq_colored
-                ]
-                ws.append(row_cells)
-            ws.append([])
-            ws.append([])
-            
-        # Final formatting of 'report' sheet
-        for col in ws.columns:
-            ws.column_dimensions[col[0].column_letter].width = 17
-
-        # For each repeat, print images on separate sheet
-        for trid in sorted(vcf_df['TRID'].unique()):
-            # Excel sheet name max 31 chars
-            sheet_name = trid[:31]  
-            ws_trid = wb.create_sheet(title=sheet_name)
-            motif_png = os.path.join(filepath, f"{sample_name}_{trid}_motifs.png")
-            meth_png  = os.path.join(filepath, f"{sample_name}_{trid}_meth.png")
-  
-            # Define starting location
-            row_pos = 1
-            col_pos = 1
-
-            for label, cell, png_path  in [("Motifs", "A3", motif_png), ("Methylation", "M3", meth_png)]:
-                if os.path.exists(png_path):
-                    # Insert a label
-                    ws_trid.cell(row=row_pos, column=col_pos, value=f"{label} {trid}")
-                    ws_trid.cell(row=row_pos, column=col_pos).font = bold_font
-                    col_pos += 12
-
-                    # Insert image and reset starting location
-                    img = Image(png_path)
-                    ws_trid.add_image(img, cell)
-                    
-        # Save workbook
-        wb.save(output_excel)
-        print(f"Report saved to {output_excel}")
+                # Insert image and reset starting location
+                img = Image(png_path)
+                ws_trid.add_image(img, cell)
+                
+    # Save workbook
+    wb.save(output_excel)
+    print(f"Report saved to {output_excel}")
 
 def main():
 
@@ -246,7 +243,14 @@ def main():
     samplesheet = sys.argv[2]
 
     samples = read_samplesheet(samplesheet)
-    write_report(samples, output_dir)
+    for sample in samples:
+        sample_name = sample['sample']
+        repeat_of_interest = sample['roi']
+        if repeat_of_interest:
+            for repeat in repeat_of_interest.split(';'):
+                write_report(sample_name=sample_name, repeat_of_interest=repeat, output_dir=output_dir)
+        else:
+            write_report(sample_name=sample_name, repeat_of_interest=None, output_dir=output_dir)
 
     end = time.time()
     print(f"\nElapsed time: {end - start}")
