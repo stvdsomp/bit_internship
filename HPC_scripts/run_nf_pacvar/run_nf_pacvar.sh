b56998d7438ac86afceb6d662658fac110c2fc45
vsc42287 vscuser <vsc42287@node4004.donphan.os>
2025-12-02 15:06:29 +0100

    updated demultiplexing scripts

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar.sh
@@ -4,7 +4,8 @@
 #qsub -I -l nodes=1:ppn=8,mem=24gb,walltime=03:00:00
 
 nextflow run stvdsomp/pacvar \
-   -r dev_personal \
+   -r dev_repeat_id \
    -latest \
+   -resume \
    -profile vsc_ugent \
    -c my_config.config
