f61e212eb1c3616d031d66f22f8a76f5e5d9a16f
vsc42287 vscuser <vsc42287@gligar09.gastly.os>
2026-01-06 08:50:08 +0100

    updated config files

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar_pbmerge_qsub.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar_pbmerge_qsub.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#PBS -l walltime=6:00:00
+#PBS -l walltime=12:00:00
 #PBS -l nodes=1:ppn=32
 #PBS -l mem=32gb
 #PBS -m abe
-#PBS -N pacvar.r84299_20251021_160234
+#PBS -N pacvar_pbmerge_demux
 
 module load Nextflow/25.04.8
 
 nextflow run stvdsomp/pacvar \
    -r dev_pbmerge_post \
-   -latest \
    -resume \
+   -latest \
    -profile vsc_ugent \
    -c "/kyukon/home/gent/422/vsc42287/BIT11/sync/HPC_scripts/run_nf_pacvar/my_config_pbmerge_qsub.config"
--- HPC_scripts/run_nf_pacvar/run_nf_pacvar_wgs_qsub.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar_wgs_qsub.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 #PBS -l walltime=6:00:00
-#PBS -l nodes=1:ppn=32
-#PBS -l mem=32gb
+#PBS -l nodes=1:ppn=96
+#PBS -l mem=64gb
 #PBS -m abe
-#PBS -N pacvar.r84299_20251021_160234
+#PBS -N pacvar_wgs_Twist
+
+export NXF_HOME=/data/gent/422/vsc42287/.nextflow
+export NXF_WORK=/data/gent/422/vsc42287/work
+export APPTAINER_CACHEDIR=/data/gent/422/vsc42287/.apptainer
+export APPTAINER_TMPDIR=/data/gent/422/vsc42287/.apptainer/tmp
 
 module load Nextflow/25.04.8
 
 nextflow run nf-core/pacvar \
    -r master \
-   -latest \
    -resume \
+   -latest \
    -profile vsc_ugent \
    -c "/kyukon/home/gent/422/vsc42287/BIT11/sync/HPC_scripts/run_nf_pacvar/my_config_wgs_qsub.config"
