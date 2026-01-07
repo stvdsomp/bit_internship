6d4273df0980be07e74d40ac1fccc96ad7028ef8
vsc42287 vscuser <vsc42287@gligar09.gastly.os>
2026-01-07 13:54:40 +0100

    update config files for pbmerge qsub test

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar_pbmerge_qsub.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar_pbmerge_qsub.sh
@@ -10,7 +10,6 @@ module load Nextflow/25.04.8
 
 nextflow run stvdsomp/pacvar \
    -r dev_pbmerge_post \
-   -resume \
    -latest \
    -profile vsc_ugent \
    -c "/kyukon/home/gent/422/vsc42287/BIT11/sync/HPC_scripts/run_nf_pacvar/my_config_pbmerge_qsub.config"
