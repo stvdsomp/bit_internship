b1be32874c9badfaaf47fc751e08bff6f46a6c95
Stijn Van de Sompele <stijn.vandesompele@ugent.be>
2026-07-29 11:03:47 +0200

    update files

--- HPC_scripts/run_nf_pacvar/run_nf_pacvar_PureTarget_1.1.0.sh
+++ HPC_scripts/run_nf_pacvar/run_nf_pacvar_PureTarget_1.1.0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=04:00:00
+#PBS -l walltime=12:00:00
 #PBS -l nodes=1:ppn=16
 #PBS -l mem=32gb
 #PBS -m abe
@@ -12,4 +12,4 @@ nextflow run stvdsomp/pacvar \
    -r dev_pbmerge_post \
    -latest \
    -profile vsc_ugent \
-   -c "/kyukon/home/gent/422/vsc42287/BIT11/sync/HPC_scripts/run_nf_pacvar/my_config_PureTarget_RUN_025.config"
+   -c "/kyukon/home/gent/422/vsc42287/BIT11/sync/HPC_scripts/run_nf_pacvar/my_config_PureTarget.config"
